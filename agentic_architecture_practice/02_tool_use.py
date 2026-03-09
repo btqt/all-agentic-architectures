553b1028cdda446c4202d524fe66085285a296ad
Phi Hoang Vu <phi.vu@lge.com>
2026-03-09 19:10:10 +0700

    update

--- agentic_architecture_practice/02_tool_use.py
+++ agentic_architecture_practice/02_tool_use.py
@@ -1,24 +1,41 @@
+import os
+
+from typing import TypedDict, Annotated
+
+from langgraph.graph import add_messages
+from langchain_core.messages import AnyMessage
+
+from duckduckgo_search import DDGS
+
 from dotenv import load_dotenv
+
 from logger_utils import get_logger
 
 logger = get_logger("tool_use")
 
-from langchain_ollama import ChatOllama
-
-from langchain_tavily import TavilySearch
-
 load_dotenv()
 
-llm = ChatOllama(model="llama3.1:8b", temperature=0.2)
-
-
-search_tool = TavilySearch(max_results=2)
-logger.info("Tavily search tool initialized.")
-
-search_tool.name = "web_search"
-search_tool.description = "A tool that can be used to search the internet for up-to-date information on any topic, including news, events, and current affairs."
+search_tool = {
+    "type": "function",
+    "function": {
+        "name": "web_search",
+        "description": "A tool that can be used to search the internet for up-to-date information on any topic, including news, events, and current affairs.",
+        "parameters": {
+            "type": "object",
+            "properties": {} 
+        }
+    }
+}
 
 tools = [search_tool]
-logger.info(f"Tool '{search_tool.name}' created with description '{search_tool.description}'")
+
+
+LLM_API_KEY = os.environ.get("LLM_API_KEY")
+LLM_MODEL_NAME = os.environ.get("LLM_MODEL_NAME")
+LLM_BASE_URL = os.environ.get("LLM_BASE_URL")
+
+class AgentState(TypedDict):
+    messages: Annotated[list[AnyMessage], add_messages]
+
 
 
